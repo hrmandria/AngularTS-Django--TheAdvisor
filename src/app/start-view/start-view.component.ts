e61d2c61e1a7d46002f74981ddc330bd965eec76
Princy.Rasonjatovo <princy.m.rasolonjatovo@gmail.com>
2021-11-20 14:56:01 +0300

    init start view component

---
+++ src/app/start-view/start-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-view',
+  templateUrl: './start-view.component.html',
+  styleUrls: ['./start-view.component.scss']
+})
+export class StartViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
