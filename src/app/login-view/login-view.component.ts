6a9f578d4aaae2660bee6d52f834d41a32e1b295
Princy.Rasonjatovo <princy.m.rasolonjatovo@gmail.com>
2021-12-08 00:34:01 +0300

    add animations(not working)

--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	trigger,
+	animate,
+	query,
+	state,
+	transition,
+	stagger,
+	style
+} from '@angular/animations';
+
+
 
 @Component({
-  selector: 'app-login-view',
-  templateUrl: './login-view.component.html',
-  styleUrls: ['./login-view.component.scss']
+	selector: 'app-login-view',
+	templateUrl: './login-view.component.html',
+	styleUrls: ['./login-view.component.scss'],
+	animations:[
+		trigger('LoginEnterState', [
+			transition('* => *', [
+				query(':enter, :leave', [
+					stagger(0, [animate('2s cubic-bezier(0.68, -0.55, 0.27, 1.55)', style({
+						transform: 'translateX(300%)',
+					}))]),
+					stagger(0, [animate('2s cubic-bezier(0.68, -0.55, 0.27, 1.55)', style({
+						transform: 'translateX(0%)',
+					}))])
+				])
+			])
+		])
+	],
+	
 })
 export class LoginViewComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
