0fdf4a9d274aa0d542b12780b34a22aa62be6269
Tolotra <tolotor@yahoo.fr>
2021-12-07 20:23:45 +0300

    Integrating the swiper.js library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SwiperModule} from "swiper/angular";
 import { StartViewComponent } from './start-view/start-view.component';
 
 const routes: Routes = [
@@ -17,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes), SwiperModule],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
