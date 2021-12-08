8fb2b65f9b496916b19a835a76e54e75e1fd18dd
hrmdbipolar <hrmandr@gmail.com>
2021-12-08 08:28:08 +0000

    'hotels'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartViewComponent } from './start-view/start-view.component';
+import { HotelViewComponent } from './hotel-view/hotel-view.component';
 
 const routes: Routes = [
 	{
@@ -12,7 +13,10 @@ const routes: Routes = [
 		path: 'start',
 		component: StartViewComponent,
 	},
-	
+	{
+		path:'locatedHotels',
+		component: HotelViewComponent,
+	}
 
 ];
 
